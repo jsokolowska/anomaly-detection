37077721694d3d1d019b34f987dd24fea1b5028e
Joanna Sokolowska <joana.sokolowska@gmail.com>
2023-05-05 22:43:53 +0200

    Clean up redundant code, fill in some LDCOF steps

--- src/clustering.py
+++ src/clustering.py
@@ -50,7 +50,6 @@ class ClusterBasedAnomalyDetection:
 
     def _ldcof(self, X):
         # TODO  perform clustering and divide clusters into large and small ones based on alpha and beta
-        self._estimator.fit(X)
         alpha = 0.9
         if "alpha" in self._measure_args.keys():
             alpha = self._measure_args["alpha"]
@@ -59,86 +58,47 @@ class ClusterBasedAnomalyDetection:
         if "beta" in self._measure_args.keys():
             beta = self._measure_args["beta"]
 
-        big_clusters = self._get_big_clusters(alpha, beta, len(X))
-
-        # TODO ref https://www.goldiges.de/publications/Anomaly_Detection_Algorithms_for_RapidMiner.pdf
-        # The LDCOF score is defined as the distance to the nearest large cluster
-        # as illustrated in Figure 1 divided by the average distance to the cluster center
-        # of the elements in that large cluster. The intuition behind this is that when
-        # small clusters are considered outlying, the elements inside the small clusters
-        # are assigned to the nearest large cluster which becomes its local neighborhood.
-        # Thus the anomaly score is computed relative to that neighborhood
-
-        #
-        scores = self._decision_function(big_clusters)
+        scores = LDCOF(alpha=alpha, beta=beta, clustering_estimator=self._estimator).decision_function(X)
         pass
 
-    def _get_big_clusters(self, alpha, beta, n_samples):
-        clusters = self._estimator.labels_
-        cluster_size = np.bincount(clusters)
-        # sort from largest
-        sorted_clusters_idx = np.argsort(cluster_size * -1)
-
-        alpha_cond_idx = []
-        beta_cond_idx = []
-
-        for i in range(1, len(np.unique(clusters))):
-            # sum clusters up to this index
-            sizes_sum = np.sum(cluster_size[sorted_clusters_idx[:i]])
-            if sizes_sum >= n_samples * alpha:
-                alpha_cond_idx.append(i)
-
-            if cluster_size[sorted_clusters_idx[i - 1]] / cluster_size[sorted_clusters_idx[i]] >= beta:
-                beta_cond_idx.append(i)
-
-        both = np.intersect1d(alpha_cond_idx, beta_cond_idx)
-
-        if len(both) > 0:
-            threshold = both[0]
-        elif len(alpha_cond_idx) > 0:
-            threshold = alpha_cond_idx[0]
-        elif len(beta_cond_idx) > 0:
-            threshold = beta_cond_idx[0]
-        else:
-            raise ValueError("Could not separate into large and small clusters")
-
-        return sorted_clusters_idx[0:threshold]
-
-    def _decision_function(self, X, big_clusters):
-        scores = np.zeros([X.shape[0],])
-        clusters = self._estimator.labels_
-
-        big_msk = np.isin(big_clusters, clusters)
-
-
-
-        return scores
-
-    def _calc_avg_dist_in_cluster(self, big):
-        pass
-
-    def _custom_measure(self, X, measure_fun):
+    def _custom_measure(self, X):
+        self._estimator.fit(X)
         # todo perform clustering
         # apply measure fun & threshold
-        pass
+        scores = self._measure(X, self._estimator.labels_)
 
-    def detect(self, X):
+        return self._measure(X, self._estimator.labels_)
+
+    def decision_fun(self, X):
         """
-        Performs anomaly detection
-
+        Perform anomaly detection and get dissimilarity scores
+        --------------------------------
         :param X: data to perform anomaly detection for
-        :return: np.ndarray of 0s (not an anomaly) and 1s (an anomaly)
+        :return: dissimilarity scores
         """
         if self._measure == "cblof":
             return self._cblof(X)
         if self._measure == "ldcof":
             return self._ldcof(X)
-        # todo custom metric usage
+
+        return self._custom_measure(X)
+
+    def detect(self, X):
+        """
+        Performs anomaly detection
+        --------------------------------
+        :param X: data to perform anomaly detection for
+        :return: np.ndarray of 0s (not an anomaly) and 1s (an anomaly)
+        """
+        scores = self.decision_fun(X)
+        return self.apply_threshold(scores)
+
+    def apply_threshold(self, scores):
         pass
 
 
-class LCDOF:
-    def __init__(self, alpha, beta, clustering_estimator):
+class LDCOF:
+    def __init__(self, alpha, beta, clustering_estimator, anomaly_threshold):
         self.alpha = alpha
         self.beta = beta
         self.clustering_estimator = clustering_estimator
@@ -149,6 +109,8 @@ class LCDOF:
         self.n_samples = None
         self.centers = None
         self.avg_dist = None
+        self.scores = None
+        self.anomaly_threshold = anomaly_threshold
 
     def score(self, X):
         self.n_samples = X.shape[0]
@@ -157,7 +119,21 @@ class LCDOF:
         self.n_clusters = np.unique(self.clusters)
 
         self._calc_big_clusters()
-        pass
+
+        # todo
+        return []
+
+    def decision_function(self, X):
+        self.clustering_estimator.fit(X)
+
+        # helper values
+        self.n_samples = X.shape[0]
+        self.clusters = self.clustering_estimator.labels_
+        self.n_clusters = np.unique(self.clusters)
+
+        self._calc_big_clusters()
+
+        return self._decision_function(X)
 
     def _calc_big_clusters(self):
         cluster_size = np.bincount(self.clusters)
@@ -190,32 +166,32 @@ class LCDOF:
         self.big_cluster_idx = self.sorted_clusters_idx[0:threshold]
 
     def _decision_function(self, X):
-        scores = np.zeros([X.shape[0], ])
-
-        big_msk = np.isin(self.big_cluster_idx, self.clusters)
-
         self._calculate_centers()
-        self._calc_avg_dist_in_cluster(X)
-        return scores
+        avg_dist = self._calc_avg_dist_in_cluster(X)
+        nearest_c = self._dist_to_nearest_large(X)
+
+        self.scores = np.divide(avg_dist / nearest_c)
 
     def _calculate_centers(self):
+        #todo
         self.centers = np.zeros([self.n_clusters, ])
         pass
 
     def _calc_avg_dist_in_cluster(self, X):
-        self.avg_dist = np.zeros([self.n_clusters, ])
+        avg_dist = np.zeros([self.n_clusters, ])
 
         for c in range(1, self.n_clusters):
             points_in_cluster = X[np.where(self.clusters == c)]
             dists = cdist(points_in_cluster, self.centers[c])
-            self.avg_dist[c] = np.average(dists)
+            avg_dist[c] = np.average(dists)
+        return avg_dist
 
     def _dist_to_nearest_large(self, X):
-        # for each point
+        dist_nearest = np.zeros([self.n_clusters, ])
+        # for each point todo
         # find distances to all clusters
         # and return minimum
         big_msk = np.isin(self.big_cluster_idx, self.clusters)
         # for big clusters this is the distance to its own cluster
 
-
-        pass
+        return dist_nearest
