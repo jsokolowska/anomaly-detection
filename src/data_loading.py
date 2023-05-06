c7d93dd55288ce84a54f33c177aa5efae2dd32e0
Joanna Sokolowska <joana.sokolowska@gmail.com>
2023-05-06 13:16:26 +0200

    Add musk loading

--- src/data_loading.py
+++ src/data_loading.py
@@ -1,7 +1,9 @@
 import pandas as pd
+from sklearn.utils import shuffle
 
+INLINERS_SIZE = 4750
+OUTLIERS_SIZE = 250
 
-# todo autodownload  & loading for both datasets
 
 def load_wine(path: str) -> (pd.DataFrame, pd.Series):
     """
@@ -21,3 +23,26 @@ def load_wine(path: str) -> (pd.DataFrame, pd.Series):
     X = wine.drop("quality", axis=1)
 
     return X.values, y.values
+
+
+def load_musk(path):
+    def map_class(molecule_name):
+        cl = molecule_name.split("-")[0]
+        if cl == "MUSK":
+            return 1
+        return 0
+
+    df = pd.read_csv(path, header=None)
+    df[0] = df[0].apply(lambda l: map_class(l))
+    df = df.drop(columns=[1])
+
+    # downsample classes
+    non_musk = df[df[0] == 1]
+    musk = df[df[0] != 1]
+
+    non_musk_downsampled = non_musk.sample(n=OUTLIERS_SIZE, random_state=58)
+    musk_downsampled = musk.sample(n=INLINERS_SIZE, random_state=12)
+    combined = pd.concat([non_musk_downsampled, musk_downsampled])
+    combined = shuffle(combined)
+
+    return combined.drop(columns=[0]), combined[0]
