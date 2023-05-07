b29f51723db344d6ffa16a45f3704173064cc715
Joanna Sokolowska <joana.sokolowska@gmail.com>
2023-05-07 08:51:28 +0200

    Split predict and decision funtion methods, code cleanup

--- src/anomaly_detection.py
+++ src/anomaly_detection.py
@@ -6,7 +6,7 @@ __all__ = ["ClusterBasedAnomalyDetection"]
 
 
 class ClusterBasedAnomalyDetection:
-    def __init__(self, clustering_estimator, dissimilarity_measure, measure_args: dict = None, contamination=0.9):
+    def __init__(self, clustering_estimator, dissimilarity_measure, measure_args: dict = None, contamination=0.1):
         """
         Class for anomaly detection based on clustering
 
@@ -15,29 +15,48 @@ class ClusterBasedAnomalyDetection:
         attribute is available it will be used for dissimilarity calculations, otherwise cluster centers will be
         calculated as mean of samples in the cluster.
         Clustering algorithms supporting outlier detection should be considered in a way that ensures no point will be
-        marked as anomaly (labels_ attribute cannot have negative numbers), f.e. for DBScan algorithm setting min_samples
-        argument to 1.
+        marked as anomaly (labels_ attribute cannot have negative numbers), f.e. for DBScan algorithm setting
+        min_samples argument to 1.
         #todo decide what to do (ask Natan & check literature)
 
-        :param dissimilarity_measure: One of: "cblof", "cds" or a function taking in data being clustered and cluster
-        information, returing dissimilarity score for each data point.
+        :param dissimilarity_measure: One of: "cblof", "cds" or a custom class.
+        Custom dissimilarity measure should have fit method taking data samples and cluster labels, and predict and
+        decision_function functions that take in only data samples.
 
-        :param measure_args: Additional arguments for cblof or ldcof measures. Ignored if callable is passed as dissimilarity
-        measure.
+        :param measure_args: Additional arguments for cblof or ldcof measures. Ignored if custom measure is used.
 
         :param contamination: Percent of data to be classified as inliners. 1-threshold will be classified as anomalies.
         """
-        self._estimator = clustering_estimator
+        self.clustering_estimator = clustering_estimator
         self._validate_measure(dissimilarity_measure)
-        self._measure = dissimilarity_measure
-        # todo actually redo usages
-        self._contamination = contamination
-        self._computed_threshold = None
+        self.measure = dissimilarity_measure
+        self.contamination = contamination
+
+        self._chosen_measure = None
 
         if measure_args is None:
             self._measure_args = {}
         else:
             self._measure_args = measure_args
+
+    def fit(self, X):
+        """
+        Performs anomaly detection and sets constants on data
+        --------------------------------
+        :param X: data to perform anomaly detection for
+        :return: np.ndarray of 0s (not an anomaly) and 1s (an anomaly)
+        """
+        if self.measure == "cblof":
+            self._chosen_measure = CBLOF(**self._measure_args, clustering_estimator=self.clustering_estimator,
+                                         contamination=self.contamination)
+        elif self.measure == "ldcof":
+            self._chosen_measure = LDCOF(**self._measure_args, clustering_estimator=self.clustering_estimator,
+                                         contamination=self.contamination)
+        else:
+            self._chosen_measure = CustomMeasure(self.clustering_estimator, self.measure)
+
+        self._chosen_measure.fit(X)
+        return self
 
     def decision_fun(self, X):
         """
@@ -46,26 +65,14 @@ class ClusterBasedAnomalyDetection:
         :param X: data to perform anomaly detection for
         :return: dissimilarity scores
         """
-        if self._measure == "cblof":
-            return self._cblof(X)
-        if self._measure == "ldcof":
-            return self._ldcof(X)
-
-        return self._custom_measure(X)
-
-    def detect(self, X):
-        """
-        Performs anomaly detection
-        --------------------------------
-        :param X: data to perform anomaly detection for
-        :return: np.ndarray of 0s (not an anomaly) and 1s (an anomaly)
-        """
-        scores = self.decision_fun(X)
-        return self._apply_threshold(scores)
+        if not self._chosen_measure:
+            raise ValueError("Classificator has not been fitted")
+        return self._chosen_measure.decision_function(X)
 
     def predict(self, X):
-        #todo implement
-        pass
+        if not self._chosen_measure:
+            raise ValueError("Classificator has not been fitted")
+        return self._chosen_measure.predict(X)
 
     def _validate_measure(self, dissimilarity_measure):
         if dissimilarity_measure == "cblof":
@@ -77,78 +84,73 @@ class ClusterBasedAnomalyDetection:
         else:
             raise ValueError(f"Expected callable or one of [cblof, ldcof], but got {dissimilarity_measure}")
 
-    def _cblof(self, X):
-        # todo handle mismatch in cluster counts & handle detected anomalies - maybe param
-        cblof_clf = CBLOF(**self._measure_args, clustering_estimator=self._estimator)
-        cblof_clf.fit(X)
-        scores = cblof_clf.decision_function(X)
-        return scores
-
-    def _ldcof(self, X):
-        alpha = 0.9
-        if "alpha" in self._measure_args.keys():
-            alpha = self._measure_args["alpha"]
-
-        beta = 5
-        if "beta" in self._measure_args.keys():
-            beta = self._measure_args["beta"]
-
-        return LDCOF(alpha=alpha, beta=beta, clustering_estimator=self._estimator,
-                     anomaly_threshold=self._contamination).score(X)
-
-    def _apply_threshold(self, scores):
-        self._computed_threshold = np.percentile(scores, 100 * self._contamination)
-        return (scores > self._computed_threshold).astype('int').ravel()
-
-    def _custom_measure(self, X):
-        self._estimator.fit(X)
-        return self._measure(X, self._estimator.labels_)
-
 
 class LDCOF:
-    def __init__(self, alpha, beta, clustering_estimator, anomaly_threshold):
+    def __init__(self, clustering_estimator, contamination, alpha=0.9, beta=5):
+        # parameters
         self.alpha = alpha
         self.beta = beta
         self.clustering_estimator = clustering_estimator
-        self.cluster_labels = None
-        self.sorted_clusters_idx = None
-        self.big_cluster_idx = None
-        self.n_clusters = None
-        self.n_samples = None
-        self.n_features = None
-        self.cluster_centers = None
-        self.avg_dist = None
-        self.scores = None
-        self.anomaly_threshold = anomaly_threshold
+        self.contamination = contamination
 
-    def score(self, X):
+        # fit results
+        self._cluster_labels = None
+        self._sorted_clusters_idx = None
+        self._big_cluster_idx = None
+        self._n_clusters = None
+        self._n_samples = None
+        self._n_features = None
+        self._cluster_centers = None
+        self._avg_cluster_dist = None
+
+    def fit(self, X):
         self.clustering_estimator.fit(X)
 
         # helper values
-        self.n_samples = X.shape[0]
-        self.n_features = X.shape[1]
-        self.cluster_labels = self.clustering_estimator.labels_
-        self.n_clusters = len(np.unique(self.cluster_labels))
+        self._n_samples = X.shape[0]
+        self._n_features = X.shape[1]
+        self._cluster_labels = self.clustering_estimator.labels_
+        self._n_clusters = len(np.unique(self._cluster_labels))
 
-        self._calc_big_clusters()
+        self._set_big_clusters()
 
+        self._calculate_centers(X)
+        self._calc_avg_dist_in_cluster(X)
+        return self
+
+    def decision_function(self, X):
+        self._is_fitted()
         return self._decision_function(X)
 
-    def _calc_big_clusters(self):
-        cluster_size = np.bincount(self.cluster_labels)
+    def predict(self, X):
+        self._is_fitted()
+        scores = self._decision_function(X)
+        return self._process_scores(scores)
+
+    def _is_fitted(self):
+        return self._big_cluster_idx & self._cluster_centers
+
+    def _calculate_threshold(self, scores):
+        self._computed_threshold = np.percentile(scores, 100 * (1 - self.contamination))
+
+    def _process_scores(self, scores):
+        return (scores > self._computed_threshold).astype('int').ravel()
+
+    def _set_big_clusters(self):
+        cluster_size = np.bincount(self._cluster_labels)
         # sort from largest
-        self.sorted_clusters_idx = np.argsort(cluster_size * -1)
+        self._sorted_clusters_idx = np.argsort(cluster_size * -1)
 
         alpha_cond_idx = []
         beta_cond_idx = []
 
-        for i in range(1, len(np.unique(self.cluster_labels))):
+        for i in range(1, len(np.unique(self._cluster_labels))):
             # sum clusters up to this index
-            sizes_sum = np.sum(cluster_size[self.sorted_clusters_idx[:i]])
-            if sizes_sum >= self.n_samples * self.alpha:
+            sizes_sum = np.sum(cluster_size[self._sorted_clusters_idx[:i]])
+            if sizes_sum >= self._n_samples * self.alpha:
                 alpha_cond_idx.append(i)
 
-            if cluster_size[self.sorted_clusters_idx[i - 1]] / cluster_size[self.sorted_clusters_idx[i]] >= self.beta:
+            if cluster_size[self._sorted_clusters_idx[i - 1]] / cluster_size[self._sorted_clusters_idx[i]] >= self.beta:
                 beta_cond_idx.append(i)
 
         both = np.intersect1d(alpha_cond_idx, beta_cond_idx)
@@ -162,40 +164,59 @@ class LDCOF:
         else:
             raise ValueError("Could not separate into large and small clusters")
 
-        self.big_cluster_idx = self.sorted_clusters_idx[0:threshold]
+        self._big_cluster_idx = self._sorted_clusters_idx[0:threshold]
 
     def _decision_function(self, X):
-        self._calculate_centers(X)
-        avg_dist = self._calc_avg_dist_in_cluster(X)
         nearest_c, nearest_idx = self._dist_to_nearest_large(X)
-        avg_dist_in_nearest = avg_dist[nearest_idx]
-        self.scores = np.divide(avg_dist_in_nearest, nearest_c)
-        return self.scores
+        return np.divide(self._avg_cluster_dist[nearest_idx], nearest_c)
 
     def _calculate_centers(self, X):
         if hasattr(self.clustering_estimator, "cluster_centers_"):
-            self.cluster_centers = self.clustering_estimator.cluster_centers_
+            self._cluster_centers = self.clustering_estimator.cluster_centers_
         else:
             # calculate centers as means of all points belonging to the cluster
-            self.cluster_centers = np.zeros([self.n_clusters, self.n_features])
-            for i in range(0, self.n_clusters):
-                self.cluster_centers[i, :] = np.mean(X[np.where(self.cluster_labels == i)], axis=0)
+            self._cluster_centers = np.zeros([self._n_clusters, self._n_features])
+            for i in range(0, self._n_clusters):
+                self._cluster_centers[i, :] = np.mean(X[np.where(self._cluster_labels == i)], axis=0)
 
     def _calc_avg_dist_in_cluster(self, X):
-        avg_dist = np.zeros([self.n_clusters, ])
+        self._avg_cluster_dist = np.zeros([self._n_clusters, ])
 
-        for c in range(0, self.n_clusters):
-            points_in_cluster = X[np.where(self.cluster_labels == c)]
-            dists = cdist(points_in_cluster, [self.cluster_centers[c]])
-            avg_dist[c] = np.average(dists)
-        return avg_dist
+        for c in range(0, self._n_clusters):
+            points_in_cluster = X[np.where(self._cluster_labels == c)]
+            dists = cdist(points_in_cluster, [self._cluster_centers[c]])
+            self._avg_cluster_dist[c] = np.average(dists)
 
     def _dist_to_nearest_large(self, X):
-        n_large_clusters = len(self.big_cluster_idx)
-        dist_all = np.zeros([self.n_samples, n_large_clusters])
+        n_large_clusters = len(self._big_cluster_idx)
+        dist_all = np.zeros([self._n_samples, n_large_clusters])
 
         for i in range(0, n_large_clusters):
-            cluster_center = self.cluster_centers[self.big_cluster_idx[i]]
+            cluster_center = self._cluster_centers[self._big_cluster_idx[i]]
             dist_all[:, i] = cdist([cluster_center], X)[0, :]
 
-        return np.min(dist_all, axis=1), self.big_cluster_idx[np.argmin(dist_all, axis=1)]
+        return np.min(dist_all, axis=1), self._big_cluster_idx[np.argmin(dist_all, axis=1)]
+
+
+class CustomMeasure:
+    def __init__(self, clustering_estimator, measure):
+        # parameters
+        self.measure = measure
+        self.clustering_estimator = clustering_estimator
+
+        # fit results
+        self._is_fitted = False
+
+    def fit(self, X):
+        self.clustering_estimator.fit(X)
+        self.measure.fit(X, self.clustering_estimator.cluster_labels_)
+        self._is_fitted = True
+        return self
+
+    def decision_function(self, X):
+        if not self._is_fitted:
+            raise ValueError("Estimator is not fitted")
+        return self.measure.decision_fun(X)
+
+    def predict(self, X):
+        return self.measure.predict(X)
